30fca28dbcb79ee12a9f8796163d0930ce28610c
M. Ali Khan <45436022+mhdl1991@users.noreply.github.com>
2019-06-10 18:05:10 +0500

    Update BrownianMotionThing.py

--- BrownianMotionThing.py
+++ BrownianMotionThing.py
@@ -12,32 +12,46 @@ window = pyglet.window.Window(width = WINDOW_WIDTH, height = WINDOW_HEIGHT)
 
 
 class BrownianSim:
-    def __init__(self, width = WINDOW_WIDTH//CELL_WIDTH, height = WINDOW_HEIGHT//CELL_HEIGHT, draw_x = 0, draw_y = 0):
+    def __init__(self, width = (WINDOW_WIDTH//CELL_WIDTH) -2, height = (WINDOW_HEIGHT//CELL_HEIGHT) -2, draw_x = CELL_WIDTH, draw_y = CELL_HEIGHT):
         self.steps = 0
         self.width, self.height = width, height
         self.draw_x, self.draw_y = draw_x, draw_y
         self.neighborhood = ((-1,0),(1,0),(0,1),(0,-1))
-        self.maxVal = 8
+        self.maxVal = 6
         self.minVal = 0
+        
+        self.wraparound = False
         
         self.randomBoard(width,height)
         self.board[height//2][width//2] = -1
-        
+      
+    def toggleBorder(self):
+        self.wraparound = not self.wraparound
         
     def emptyBoard(self, width = WINDOW_WIDTH//CELL_WIDTH, height = WINDOW_HEIGHT//CELL_HEIGHT):
         self.board = np.zeros((height, width), dtype = int)
         
     def randomBoard(self, width = WINDOW_WIDTH//CELL_WIDTH, height = WINDOW_HEIGHT//CELL_HEIGHT):
-        self.board = (np.random.random((height, width)) > 0.99).astype(int) * 8
+        self.board = (np.random.random((height, width)) > 0.96).astype(int) * self.maxVal
 
-    def chooseDirection(self):
-        return random.choice(self.neighborhood)
-        
     def getDestination(self,x,y,dx,dy):
-        return (x + dx + self.width)%self.width, (y + dy + self.height)%self.height
+        if self.wraparound:
+            return (x + dx + self.width)%self.width, (y + dy + self.height)%self.height
+        else:
+            return max(0, min(x + dx, self.width-1)), max(0, min(y + dy, self.height-1))
+            
+    def getDestinationValue(self,x,y,dx,dy):
+        x2, y2 = self.getDestination(x,y,dx,dy)
+        return self.board[y2][x2]
+        
+    def destinationIsWithinBounds(self,x,y,dx,dy):
+        return 0<=x+dx<self.width and 0<=y+dy<self.height
 
     def getListNeighborsCoords(self,x,y):
-        return [self.getDestination(x,y,dx,dy) for dx,dy in self.neighborhood]
+        if self.wraparound:
+            return [self.getDestination(x,y,dx,dy) for dx,dy in self.neighborhood]
+        else:
+            return [self.getDestination(x,y,dx,dy) for dx,dy in self.neighborhood if self.destinationIsWithinBounds(x,y,dx,dy)]
         
     def getListNeighbors(self,x,y):
         return [self.board[j][i] for i,j in self.getListNeighborsCoords(x,y)]
@@ -49,21 +63,26 @@ class BrownianSim:
                 drawBlock = False
                 
                 if currentCell > 0:
-                    v = int( (currentCell / self.maxVal) * 192)
-                    drawColor = (64 + v,0,0)
+                    v = int( (currentCell / self.maxVal) * 255)
+                    drawColor = (v,0,0)
                     drawBlock = True
                 elif currentCell < 0:
                     drawBlock = True
                     b = currentCell * 16 #currentCell has values -1 to -5
                     drawColor = (0,240 + b,175 - b);  #ICE
                     
-                    
                 if drawBlock:
                     X1,Y1 = self.draw_x + (x * CELL_WIDTH), self.draw_y + (y * CELL_HEIGHT),
                     X2,Y2 = X1 + CELL_WIDTH,Y1 + CELL_HEIGHT
                     pyglet.graphics.draw(4 ,pyglet.gl.GL_POLYGON, ('v2i',[X1,Y1, X2,Y1, X2,Y2, X1,Y2] ), ('c3B', drawColor * 4 ) )
                     
-                    
+        #DRAW BORDER
+        if not self.wraparound:
+            X1,Y1 = self.draw_x, self.draw_y,
+            X2,Y2 = X1 + (CELL_WIDTH*self.width),Y1 + (CELL_HEIGHT*self.height)
+            pyglet.graphics.draw(8 ,pyglet.gl.GL_LINES, ('v2i',[X1,Y1, X2,Y1,  X2,Y1, X2,Y2,  X1,Y2, X2,Y2,  X1,Y1, X1,Y2] ), ('c3B', (255,255,255)* 8 ) )
+          
+          
     def updateBoard(self):
         newBoard = np.copy(self.board)
         for y in range(self.height):
@@ -71,13 +90,17 @@ class BrownianSim:
                 currentCell = self.board[y][x]
                 listNeighbors = self.getListNeighbors(x,y)
                 
+                #AVOID MOVING INTO FREEZY BLOCKS
+                if not self.wraparound:
+                    validDirections = [n for n in self.neighborhood if self.destinationIsWithinBounds(x,y,n[0],n[1]) and self.getDestinationValue(x,y,n[0],n[1])>-1]
+                else:
+                    validDirections = [n for n in self.neighborhood if self.getDestinationValue(x,y,n[0],n[1])>-1]
+                    
                 #FREEZE
                 freezeNearby = sum([1 for neighbor in listNeighbors if neighbor < 0])
-                
-                if currentCell > 0:
-                    if freezeNearby > 0: 
-                        newBoard[y][x] = -1
-                        currentCell = -1
+                if currentCell > 0 and freezeNearby: 
+                    newBoard[y][x] = -1
+                    currentCell = -1
                         
                 #AESTHETIC FREEZING        
                 if currentCell < 0:
@@ -86,25 +109,22 @@ class BrownianSim:
                     if freezeNearby == 2: newBoard[y][x] = -3; currentCell = -3
                     if freezeNearby == 3: newBoard[y][x] = -4; currentCell = -4
                     if freezeNearby == 4: newBoard[y][x] = -5; currentCell = -5
-                
-                #DIFFUSION
-                if currentCell > 1 and random.random() > 0.6:
-                    for x2,y2 in self.getListNeighborsCoords(x,y):
-                        if self.board[y2][x2] == -1: continue #Avoid grid cells marked with negative numbers 
-                        
-                        if self.board[y2][x2] + 1 < self.maxVal:
-                            if currentCell: 
-                                newBoard[y][x] -= 1
-                                newBoard[y2][x2] += 1
-                                currentCell -= 1 
-
-                                
-                #MOVEMENT
-                if currentCell > 0: 
-                    dx, dy = self.chooseDirection()
-                    x2, y2 = self.getDestination(x,y,dx,dy)
-                    if self.board[y2][x2] > -1: #Avoid grid cells marked with negative numbers 
-                        if self.board[y2][x2] + currentCell < self.maxVal:
+                            
+                if validDirections:
+                    #DIFFUSION
+                    if currentCell > 1 and random.random() > 0.5:    
+                        for dx,dy in validDirections:
+                            x2,y2 = self.getDestination(x,y,dx,dy)
+                            if self.board[y2][x2] + 1 <= self.maxVal:
+                                if currentCell: 
+                                    newBoard[y][x] -= 1
+                                    newBoard[y2][x2] += 1
+                                    currentCell -= 1                                 
+                    #MOVEMENT
+                    if currentCell > 0: 
+                        dx, dy = random.choice(validDirections)
+                        x2, y2 = self.getDestination(x,y,dx,dy)
+                        if self.board[y2][x2] + currentCell <= self.maxVal:
                             newBoard[y][x] -= currentCell
                             newBoard[y2][x2] += currentCell
                             currentCell = 0 #This cell is now empty
@@ -124,11 +144,17 @@ def on_draw():
 def on_key_press(symbol, modifiers):
     global RUN
     if symbol == key.P:
+        #PAUSE SIMULATION
         RUN = not RUN
     elif symbol == key.R:
+        #RANDOMIZE THE BOARD
         TEST.randomBoard()
     elif symbol == key.E:
+        #CLEAR THE BOARD
         TEST.emptyBoard()
+    elif symbol == key.W:
+        #TOGGLE BORDER
+        TEST.toggleBorder()
     pass
     
     
