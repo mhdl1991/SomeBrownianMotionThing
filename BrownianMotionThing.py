c76e6d88954cdf28315dae4e4a16124458eb4bd9
M. Ali Khan <45436022+mhdl1991@users.noreply.github.com>
2019-06-10 11:23:38 +0500

    Update BrownianMotionThing.py
    changes to physics

--- BrownianMotionThing.py
+++ BrownianMotionThing.py
@@ -5,22 +5,24 @@ from pyglet.window import mouse, key
 
 WINDOW_WIDTH = 800
 WINDOW_HEIGHT = 480
-CELL_WIDTH = 16
-CELL_HEIGHT = 16
+CELL_WIDTH = 10
+CELL_HEIGHT = 10
 RUN = True
 window = pyglet.window.Window(width = WINDOW_WIDTH, height = WINDOW_HEIGHT)
 
 
 class BrownianSim:
-    def __init__(self, width = WINDOW_WIDTH//CELL_WIDTH, height = WINDOW_HEIGHT//CELL_HEIGHT):
+    def __init__(self, width = WINDOW_WIDTH//CELL_WIDTH, height = WINDOW_HEIGHT//CELL_HEIGHT, draw_x = 0, draw_y = 0):
         self.steps = 0
         self.width, self.height = width, height
+        self.draw_x, self.draw_y = draw_x, draw_y
         self.neighborhood = ((-1,0),(1,0),(0,1),(0,-1))
         self.maxVal = 16
         self.minVal = 0
         
+        
         self.emptyBoard(width,height)
-        self.board[height//2][width//2] = self.maxVal
+        self.board[height//2][width//2] = -2
         
         
     def emptyBoard(self, width = WINDOW_WIDTH//CELL_WIDTH, height = WINDOW_HEIGHT//CELL_HEIGHT):
@@ -35,45 +37,67 @@ class BrownianSim:
     def getDestination(self,x,y,dx,dy):
         return (x + dx + self.width)%self.width, (y + dy + self.height)%self.height
 
+    def getListNeighbors(self,x,y):
+        list_coords = [self.getDestination(x,y,dx,dy) for dx,dy in self.neighborhood]
+        return [self.board[j][i] for i,j in list_coords]
+
     def drawBoard(self):
         for y in range(self.height):
             for x in range(self.width):
                 currentCell = self.board[y][x]
-                if currentCell:
+                drawBlock = False
+                
+                if currentCell > 0:
                     v = int( (currentCell / self.maxVal) * 255)
-                    drawColor = (v,v,v)
-                    X1,Y1 = x * CELL_WIDTH, y * CELL_HEIGHT,
+                    drawColor = (160,v,0)
+                    drawBlock = True
+                else:
+                    if currentCell == -1: drawColor = (240,240,240); drawBlock = True #SOLID
+                    if currentCell == -2: drawColor = (0,255,255); drawBlock = True #ICE
+                    
+                if drawBlock:
+                    X1,Y1 = self.draw_x + (x * CELL_WIDTH), self.draw_y + (y * CELL_HEIGHT),
                     X2,Y2 = X1 + CELL_WIDTH,Y1 + CELL_HEIGHT
                     pyglet.graphics.draw(4 ,pyglet.gl.GL_POLYGON, ('v2i',[X1,Y1, X2,Y1, X2,Y2, X1,Y2] ), ('c3B', drawColor * 4 ) )
+                    
                     
     def updateBoard(self):
         newBoard = np.copy(self.board)
         for y in range(self.height):
             for x in range(self.width):
                 currentCell = self.board[y][x]
+                listNeighbors = self.getListNeighbors(x,y)
                 
-                if currentCell and random.random() > 0.5:
-                    #MOVE
-                    dx, dy = self.chooseDirection()
-                    x2, y2 = self.getDestination(x,y,dx,dy)
-                    #if currentCell + self.board[y2][x2] <= self.maxVal:
-                    newBoard[y][x] -= currentCell
-                    newBoard[y2][x2] += currentCell
-                    currentCell = 0 #This cell is now empty
+                #FREEZE
+                if currentCell > 0:
+                    freezeNearby = sum([1 for neighbor in listNeighbors if neighbor == -2])
+                    if freezeNearby: 
+                        newBoard[y][x] = -2
+                        currentCell = -2
                 
-                if currentCell > self.maxVal:
-                    #DIFFUSE
+                #DIFFUSION
+                if currentCell > 1:
                     for dx,dy in self.neighborhood:
                         x2, y2 = self.getDestination(x,y,dx,dy)
-                        if self.board[y2][x2] + 1 < self.maxVal:
-                            currentCell -= 1
-                            newBoard[y][x] -= 1
-                            newBoard[y2][x2] += 1
-                            
-                        if not currentCell:
-                            break
-
+                        if self.board[y2][x2] > -1: #Avoid grid cells marked with negative numbers 
+                            if self.board[y2][x2] + 1 < self.maxVal:
+                                currentCell -= 1
+                                newBoard[y][x] -= 1
+                                newBoard[y2][x2] += 1    
+                                
+                        if not currentCell: break
                 
+                #MOVEMENT
+                if currentCell > 0: 
+                    dx, dy = self.chooseDirection()
+                    x2, y2 = self.getDestination(x,y,dx,dy)
+                    if self.board[y2][x2] > -1: #Avoid grid cells marked with negative numbers 
+                        if self.board[y2][x2] + currentCell < self.maxVal:
+                            newBoard[y][x] -= currentCell
+                            newBoard[y2][x2] += currentCell
+                            currentCell = 0 #This cell is now empty
+
+                        
         self.board = newBoard
         self.steps += 1
 
@@ -102,7 +126,10 @@ def on_mouse_press(x, y, button, modifiers):
     if button == mouse.LEFT:
         TEST.board[_y][_x] = TEST.maxVal
     elif button == mouse.RIGHT:
-        TEST.board[_y][_x] = TEST.minVal
+        if (TEST.board[_y][_x] == -2): 
+            TEST.board[_y][_x] = 0
+        else:
+            TEST.board[_y][_x] = -2
            
 def update(t):
     global RUN
